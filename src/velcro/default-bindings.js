ac18c5ed160083ed9d3d6c124510808e109587eb
Trey Shugart <treshugart@gmail.com>
2013-04-11 11:04:38 +1000

    Add bootstrap for examples.

--- src/velcro/default-bindings.js
+++ src/velcro/default-bindings.js
@@ -1,59 +1,58 @@
-var context = function(app, element, options) {
-    if (!options.context) {
-        Velcro.utils.throwForElement(element, 'A context option must be specified.');
-    }
-
-    app.context(options.context);
-};
-
-var include = function(app, element, options) {
-    options = Velcro.utils.merge({
-        path: '',
-        context: false,
-        callback: function(){},
-        view: {}
-    }, options);
-
-    var view = new Velcro.View(options.view);
-
-    view.options.target = element;
-
-    if (typeof options.context === 'function') {
-        options.context = options.context();
-    }
-
-    if (!options.path) {
-        Velcro.utils.throwForElement(element, 'A path option must be specified.');
-    }
-
-    view.render(options.path, function() {
-        app.bindDescendants(element, options.context);
-        options.callback();
-    });
-};
-
-var routable = function(app, element, options) {
-    var router = options.router;
-
-    if (!router) {
-        Velcro.utils.throwForElement(element, 'Cannot bind router "' + value + '" because it does not exist.');
-    }
-
-    if (!router instanceof Velcro.Router) {
-        Velcro.utils.throwForElement(element, 'Cannot bind router "' + value + '" because it is not an instanceof "Velcro.Router".');
-    }
-
-    router.view.options.target = element;
-    router.bind();
-};
-
-var text = function(app, element, options) {
-    element.innerText = options.text;
-};
-
 Velcro.defaultBindings = {
-    context: context,
-    include: include,
-    routable: routable,
-    text: text
+    context: function(app, element, options) {
+        if (!options.context) {
+            Velcro.utils.throwForElement(element, 'A context option must be specified.');
+        }
+
+        app.context(options.context);
+    },
+
+    each: function(app, element, options) {
+        var clone = Velcro.utils.html(element);
+    },
+
+    include: function(app, element, options) {
+        options = Velcro.utils.merge({
+            path: '',
+            context: false,
+            callback: function(){},
+            view: {}
+        }, options);
+
+        var view = new Velcro.View(options.view);
+
+        view.options.target = element;
+
+        if (typeof options.context === 'function') {
+            options.context = options.context();
+        }
+
+        if (!options.path) {
+            Velcro.utils.throwForElement(element, 'A path option must be specified.');
+        }
+
+        view.render(options.path, function() {
+            app.bindDescendants(element, options.context);
+            options.callback();
+        });
+    },
+
+    routable: function(app, element, options) {
+        var router = options.router;
+
+        if (!router) {
+            Velcro.utils.throwForElement(element, 'Cannot bind router "' + value + '" because it does not exist.');
+        }
+
+        if (!router instanceof Velcro.Router) {
+            Velcro.utils.throwForElement(element, 'Cannot bind router "' + value + '" because it is not an instanceof "Velcro.Router".');
+        }
+
+        router.view.options.target = element;
+        router.bind();
+    },
+
+    text: function(app, element, options) {
+        element.innerText = options.text;
+    }
 };
