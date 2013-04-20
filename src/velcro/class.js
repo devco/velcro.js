2570dcdf8d1b59f912ceee9fad81e23fd075ff59
Trey Shugart <treshugart@gmail.com>
2013-04-21 09:55:55 +1000

    Clean up class factory and make router components classes.

--- src/velcro/class.js
+++ src/velcro/class.js
@@ -1,69 +1,73 @@
 (function() {
-    velcro.Class = function() {};
-
     var extending  = false;
     var classes    = [];
     var extensions = {};
 
+    velcro.Class = function() {};
     velcro.Class.extend = function(definition) {
-        var Child = createChild();
-        register(this, Child);
-        applyPrototype(this, Child);
+        var $this  = this;
+        var Child  = createConstructor();
+        var extend = arguments.callee;
+
+        registerHierarchy();
+        applyPrototype();
         applyDefinition(this, Child, arguments.callee, definition);
+
         return Child;
+
+
+        function applyPrototype() {
+            extending = true;
+            Child.prototype = new $this();
+            extending = false;
+        }
+
+        function applyDefinition() {
+            Child.extend = extend;
+
+            Child.isSubClassOf = function(Ancestor) {
+                var Parent = extensions[classes.indexOf(Child)];
+                return Parent === velcro.Class || Parent === Ancestor || Parent.isSubClassOf(Ancestor);
+            };
+
+            for (var member in definition) {
+                if (typeof Child.prototype[member] === 'function') {
+                    Child.prototype[member] = createProxy(member);
+                } else {
+                    Child.prototype[member] = definition[member];
+                }
+            }
+
+            Child.prototype.constructor = Child;
+        }
+
+        function createProxy(method) {
+            return (function(method, fn) {
+                return function() {
+                    var tmp = this.$super;
+
+                    this.$super = $this.prototype[method];
+
+                    var ret = fn.apply(this, arguments);
+
+                    this.$super = tmp;
+
+                    return ret;
+                };
+            })(method, definition[method]);
+        }
+
+        function registerHierarchy() {
+            classes.push(Child);
+            extensions[classes.indexOf(Child)] = $this;
+        }
     };
 
-    function createChild() {
+    function createConstructor() {
         return function() {
             if (!extending && typeof this.init === 'function') {
                 this.init.apply(this, arguments);
             }
         };
-    }
-
-    function applyPrototype(Parent, Child) {
-        extending = true;
-        Child.prototype = new Parent();
-        extending = false;
-    }
-
-    function applyDefinition(Parent, Child, extend, definition) {
-        Child.extend = extend;
-
-        Child.isSubClassOf = function(Ancestor) {
-            var Parent = extensions[classes.indexOf(Child)];
-            return Parent === velcro.Class || Parent === Ancestor || Parent.isSubClassOf(Ancestor);
-        };
-
-        for (var i in definition) {
-            if (typeof Child.prototype[i] === 'function') {
-                Child.prototype[i] = createProxy(Parent.prototype, definition, i);
-            } else {
-                Child.prototype[i] = definition[i];
-            }
-        }
-
-        Child.prototype.constructor = Child;
-    }
-
-    function createProxy(prototype, definition, name) {
-        return (function(name, fn) {
-            return function() {
-                var tmp = this.$super;
-
-                this.$super = prototype[name];
-
-                var ret = fn.apply(this, arguments);
-
-                this.$super = tmp;
-
-                return ret;
-            };
-        })(name, definition[name]);
-    }
-
-    function register(Parent, Child) {
-        classes.push(Child);
-        extensions[classes.indexOf(Child)] = Parent;
     }
 })();
