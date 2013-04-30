e7c276922c119997ede754cd245f1b06b07162fc
Trey Shugart <treshugart@gmail.com>
2013-04-30 15:55:40 +1000

    Enhance the 'with' binding so that it can take a function so it can be bound to a controller.

--- dist/velcro.js
+++ dist/velcro.js
@@ -2016,7 +2016,17 @@
                 vc.utils.throwForElement(element, 'A context option must be specified.');
             }
 
-            app.context(options.context);
+            var context = options.context;
+
+            if (typeof options.context === 'function') {
+                context = options.context();
+            }
+
+            if (typeof options.context !== 'object') {
+                vc.utils.throwForElement(element, 'The context option must either be a function that returns an object or an object itself.');
+            }
+
+            app.context(context);
         };
     };
 })();
