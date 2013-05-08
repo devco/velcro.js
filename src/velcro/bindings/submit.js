eea0b68b0408edc0b1b9c4ca56d8dcbb385fe987
Trey Shugart <treshugart@gmail.com>
2013-05-08 12:01:08 +1000

    Improve error reporting on submit binding.

--- src/velcro/bindings/submit.js
+++ src/velcro/bindings/submit.js
@@ -2,6 +2,12 @@
     vc.bindings.vc.submit = function(app, element) {
         var dom = vc.dom(element);
 
+        this.options = {
+            callback: function() {
+                vc.utils.throwForElement(element, 'You must specify a callback to the "submit" binding.');
+            }
+        };
+
         this.init = function(options, bindings) {
             dom.on('submit', function(e) {
                 if (options.callback() !== true) {
